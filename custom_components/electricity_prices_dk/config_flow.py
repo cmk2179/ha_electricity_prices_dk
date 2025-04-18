1d724ca298ce02ea657ae1deb7481666f0536700
Chris Opstrup <ckjaersig@gmail.com>
2025-04-18 12:50:42 +0200

    Add license

--- custom_components/electricity_prices_dk/config_flow.py
+++ custom_components/electricity_prices_dk/config_flow.py
@@ -1,3 +1,19 @@
+# An integration for Home Assistant that fetches electricity prices and makes the data available via sensors.
+# Copyright (C) 2025  Chris Opstrup
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import voluptuous as vol
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 from homeassistant.core import callback
